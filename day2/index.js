edded409c6171cffacc442cbf726293bd8299c7c
Logan Lampton <lampton.logan@gmail.com>
2022-12-05 17:09:23 -0500

    innerHTML reflects JS

--- day2/index.js
+++ day2/index.js
@@ -1,10 +1,18 @@
 import userData from "./data.js";
 
+const main = document.body.querySelector('main')
+
 function transformData(data) {
     const peopleArr = data.map(({name, dob}) => {
         const fullName = `${name.first} ${name.last}`
         const birthday = new Date(dob.date).toDateString()
-        return {fullName, birthday}       
+        main.innerHTML += `
+        <div class="person">
+            <p>${fullName}</p>
+            <p>${birthday}</p>
+        </div>
+        `
+        return {fullName, birthday}      
     })
     return peopleArr;
 }
