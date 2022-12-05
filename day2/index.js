bc3afb072829bc22123c107a6ea3eb461883dd12
Logan Lampton <lampton.logan@gmail.com>
2022-12-05 16:38:37 -0500

    day2 code

---
+++ day2/index.js
@@ -0,0 +1,20 @@
+import userData from "./data.js";
+
+const main = document.body.querySelector('main')
+
+function transformData(data) {
+    const peopleArr = data.map(({name, dob}) => {
+        const fullName = `${name.first} ${name.last}`
+        const birthday = new Date(dob.date).toDateString()
+        main.innerHTML += `
+        <div class="person">
+            <p>${fullName}</p>
+            <p>${birthday}</p>
+        </div>
+        `
+        return {fullName, birthday}       
+    })
+    return peopleArr;
+}
+
+console.log(transformData(userData));
