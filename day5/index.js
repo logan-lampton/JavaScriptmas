b5ba8c105a910df19d752ec5a1cd98a95ca9f9ad
Logan Lampton <lampton.logan@gmail.com>
2022-12-06 16:57:28 -0500

    solved day5

--- day5/index.js
+++ day5/index.js
@@ -17,6 +17,25 @@ import products from "./data.js";
     ]
 */
 
+const body = document.querySelector("body");
+
 function getSaleItems(data){
-    
+    //only sweets
+    let shoppingCart = data.filter(({item, price, type}) => {
+        return type === "sweet";
+    }).map(({item, price, type}) => {
+        return {item, price};
+    })
+    //render to DOM
+    shoppingCart.forEach(({item, price}) => {
+        body.innerHTML +=
+            `
+            <div class="candy">
+                <p>${item}</p>
+                <p>$${price}</p>
+            </div>
+            `
+    })
 };
+
+getSaleItems(products);
